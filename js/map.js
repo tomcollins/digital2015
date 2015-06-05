ffa3f6fe2ffa8125015a46a7247f111a317d1461
Tom Collins <tom.collins@bbc.co.uk>
2015-06-05 13:51:02 +0100

    Adding marker to map.

--- js/map.js
+++ js/map.js
@@ -7,5 +7,16 @@ function initialize() {
     zoom: 8
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  /*
+  var marker = new google.maps.Marker({
+      position: { 
+        lat: 51.619100894000624, 
+        lng: -3.9980129258861368
+      },
+      map: map,
+      title: 'Hello Digital 2015'
+  });
+  */
 }
 google.maps.event.addDomListener(window, 'load', initialize);
